328da396a8b80c2ad52b5a715f5a75982b7b5329
Andrew Brehaut <andrew@brehaut.net>
2012-12-09 17:26:16 +1300

    0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-pingback "0.2.0-SNAPSHOT"
+(defproject clj-pingback "0.2.0"
   :description "An implementation of Pingback for clojure"
   :dependencies {org.clojure/clojure "1.4.0",
                  clj-http "0.5.8",
